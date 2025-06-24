b1bd35aee12c3bde4f255ccd8dd989f79e53027a
Pedro Clerici <pedro.clerici@protonmail.com>
2025-06-24 07:10:44 -0300

    feat: github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pedroclerici.github.io',
+  base: 'mark-portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
